1bb44969f3cda4c3420ff62bbb8f19bfbf5f2673
Varun Prajapati <varunprajapati123@gmail.com>
2024-04-05 19:25:34 +0530

    event emitter

--- 1/8_event_emitter.js
+++ 1/8_event_emitter.js
@@ -1,13 +1,21 @@
-const EventEmitter = require("events");
+// get back the class
+// if want custom extend from class
+// otherwise just for emitting and handling events create instance
+const EventEmitter = require('events')
 
-const customEmitter = new EventEmitter();
+const customEmitter = new EventEmitter()
 
-customEmitter.on("response", (name, id)=> {
-    console.log(`Data recieved with name ${name} and id: ${id}`);
-});
+// on and emit methods
+// keep track of the order
+// additional arguments
+// built-in modules utilize it
 
-customEmitter.on("response", () => {
-    console.log(`some other logic here`);
-});
+customEmitter.on('response', (name, id) => {
+  console.log(`data recieved user ${name} with id:${id}`)
+})
 
-customEmitter.emit("response", "Varun", "23");
+customEmitter.on('response', () => {
+  console.log('some other logic here')
+})
+
+customEmitter.emit('response', 'Varun', 23)
